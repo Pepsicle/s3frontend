8cd7888a5d0971c4e71b4320d846cd2c6fb4f79a
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-12-15 11:38:34 +0100

    Added working add user function

--- src/components/JoinTournamentModal.vue
+++ src/components/JoinTournamentModal.vue
@@ -1,11 +1,19 @@
 <template>
-  <button type="button" class="btn btn-primary activateModal" @click="modal.show()">Sign up for {{modalTournament.tournamentDescription}}</button>
+  <button
+    type="button"
+    class="btn btn-primary activateModal"
+    @click="modal.show()"
+  >
+    Sign up for {{ modalTournament.tournamentDescription }}
+  </button>
 
   <div class="modal fade" ref="Component" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Signupform {{modalTournament.tournamentDescription}}</h5>
+          <h5 class="modal-title" id="exampleModalLabel">
+            Signupform {{ modalTournament.tournamentDescription }}
+          </h5>
           <button
             type="button"
             class="btn-close"
@@ -14,12 +22,13 @@
           ></button>
         </div>
         <div class="modal-body">
-          <h5>Enter playername here:</h5><br>
-          <form>
-            <input type="text" v-model="userName">
-          </form><br>
+          <h5>Enter playername here:</h5>
+          <br />
+          <input type="text" v-model="userName" /><br />
           <div>
-            <button class="btn btn-primary" v-on:click="AddUserToTournament">Sign up</button>
+            <button class="btn btn-primary" v-on:click="AddUserToTournament">
+              Sign up
+            </button>
           </div>
         </div>
       </div>
@@ -28,15 +37,15 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import { Modal } from "bootstrap";
 
 export default {
   name: "Modal",
   data: () => ({
     modal: null,
-    componentcount: '',
-    userName: '',
+    componentcount: "",
+    userName: "",
   }),
   props: {
     modalTournament: {
@@ -44,30 +53,39 @@ export default {
     },
     modalContent: {
       default: null,
-      type: Array
+      type: Array,
     },
     modalType: {
       default: null,
-      type: String
-    }
+      type: String,
+    },
   },
   mounted() {
     this.modal = new Modal(this.$refs.Component);
   },
   methods: {
     async AddUserToTournament() {
-        try {const res = await axios
-                    .get('https://localhost:44315/Tournament/GetTournamentUsers?userName=' + this.userName + '&tournamentId=' + this.modalTournament.tournamentId)
-                    .then((result) => {
-                    return result.data;
-                    });
-                this.tournamentUsers = res
-                this.loaded = true
-            } catch (error){
-                console.log(error)
-            }
-            this.loaded = true;
-    }
-  }
+      console.log("adding user");
+      try {
+        const res = await axios
+          .post(
+            "https://localhost:44315/Tournament/AddPlayerToTournament?userName=" + this.userName + "&tournamentId=" + this.modalTournament.tournamentId
+          )
+          .then((result) => {
+            return result.data;
+          });
+        if(res == 200) {
+          console.log("Redirecting...")
+          this.modal.hide()
+          this.$router.push('Tournaments')
+        } else {
+          console.log("error")
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.loaded = true;
+    },
+  },
 };
 </script>
